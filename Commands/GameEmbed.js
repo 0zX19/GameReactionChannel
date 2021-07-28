85823600fd5f0adc2e27a2a077b276b1cf8681f0
Noodle <75095826+FandyAnon@users.noreply.github.com>
2021-07-28 11:22:00 +0800

    Games picker

--- Commands/GameEmbed.js
+++ Commands/GameEmbed.js
@@ -1,1 +1,24 @@
+const Discord = require('discord.js')
 
+module.exports = {
+	name: 'gameembed',
+	description: 'gameembed',
+	aliases: ['gameembed'],
+	usage: `Embed <text>`,
+	cooldown: 5,
+	async execute(client, message, args) { 
+
+
+let Embedtool = new Discord.MessageEmbed()
+.setTitle("Pick the Game")
+.setDescription(`
+1️⃣ Valorant
+2️⃣ Ragnarok
+3️⃣ Minecraft
+const games = await message.channel.send(EmbedTool)
+games.react("1️⃣")
+games.react("2️⃣")
+games.react("3️⃣")
+
+  }
+}
